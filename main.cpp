6b0a29fc325aff6521820735589e37261625205b
Nicholas Barbosa <54655396+nicholasbarbosa@users.noreply.github.com>
2021-05-01 16:21:10 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -12,13 +12,67 @@
 #include <glm/ext/matrix_transform.hpp>  // glm::translate, glm::rotate, glm::scale
 #include <glm/ext/matrix_clip_space.hpp> // glm::perspective
 #include <glm/ext/scalar_constants.hpp>  // glm::pi
+int velA = 0;
+//vetores do aviao
+std::vector<glm::vec3> aviao_v;
+std::vector<glm::vec2> aviao_t;
+std::vector<glm::vec3> aviao_n;
 
-std::vector<glm::vec3> out_vertices;
-std::vector<glm::vec2> out_texcoord;
-std::vector<glm::vec3> out_normals;
+//vetores chao
+std::vector<glm::vec3> chao_v;
+std::vector<glm::vec2> chao_t;
+std::vector<glm::vec3> chao_n;
 
-bool parser(std::vector<glm::vec3> &out_vertices, std::vector<glm::vec2> &out_texcoord, std::vector<glm::vec3> &out_normals, std::string file_name)
+//vetores das arvores
+std::vector<glm::vec3> a1_v;
+std::vector<glm::vec2> a1_t;
+std::vector<glm::vec3> a1_n;
+
+std::vector<glm::vec3> a2_v;
+std::vector<glm::vec2> a2_t;
+std::vector<glm::vec3> a2_n;
+
+std::vector<glm::vec3> a3_v;
+std::vector<glm::vec2> a3_t;
+std::vector<glm::vec3> a3_n;
+
+//vetores das estruturas
+std::vector<glm::vec3> c1_v;
+std::vector<glm::vec2> c1_t;
+std::vector<glm::vec3> c1_n;
+
+std::vector<glm::vec3> c2_v;
+std::vector<glm::vec2> c2_t;
+std::vector<glm::vec3> c2_n;
+
+//vetores dos animais
+std::vector<glm::vec3> animal1_v;
+std::vector<glm::vec2> animal1_t;
+std::vector<glm::vec3> animal1_n;
+
+std::vector<glm::vec3> animal2_v;
+std::vector<glm::vec2> animal2_t;
+std::vector<glm::vec3> animal2_n;
+
+std::vector<glm::vec3> animal3_v;
+std::vector<glm::vec2> animal3_t;
+std::vector<glm::vec3> animal3_n;
+
+//montanhas
+std::vector<glm::vec3> m1_v;
+std::vector<glm::vec2> m1_t;
+std::vector<glm::vec3> m1_n;
+
+std::vector<glm::vec3> m2_v;
+std::vector<glm::vec2> m2_t;
+std::vector<glm::vec3> m2_n;
+
+
+/*--------------------------------------------------------------------------------------------------*/
+
+void parser(std::vector<glm::vec3> &out_vertices, std::vector<glm::vec2> &out_texcoord, std::vector<glm::vec3> &out_normals, std::string file_name)
 {
+    if(out_vertices.size() != 0) return;
     //vetor que guarda as posições que vamos usar de fato
 
     //vetor temporario para ler o arquivo
@@ -127,8 +181,6 @@ bool parser(std::vector<glm::vec3> &out_vertices, std::vector<glm::vec2> &out_te
         glm::vec3 vertex = vertex_normals[vertexIndex - 1];
         out_normals.push_back(vertex);
     }
-
-    return true;
 }
 
 void desenhaOBJ(const std::vector<glm::vec3> &out_vertices, const std::vector<glm::vec2> &out_texcoord, const std::vector<glm::vec3> &out_normals){
@@ -147,31 +199,110 @@ void desenhaOBJ(const std::vector<glm::vec3> &out_vertices, const std::vector<gl
 }
 
 void drawAviao(){
-    std::vector<glm::vec3> out_vertices;
-    std::vector<glm::vec2> out_texcoord;
-    std::vector<glm::vec3> out_normals;
+    parser(aviao_v, aviao_t, aviao_n, "aviao2.obj");
 
-    parser(out_vertices, out_texcoord, out_normals, "aviao2.obj");
+    glColor3f(0,1,0);
+    glPushMatrix();
+        glTranslated(0,0,-velA);
+        desenhaOBJ(aviao_v, aviao_t, aviao_n);
+    glPopMatrix();
 
-    glColor3f(0,0,0);
-    desenhaOBJ(out_vertices, out_texcoord, out_normals);
+    glutPostRedisplay();
 }
 
 void drawGramSky(){
-    std::vector<glm::vec3> out_vertices;
-    std::vector<glm::vec2> out_texcoord;
-    std::vector<glm::vec3> out_normals;
-
-    parser(out_vertices, out_texcoord, out_normals, "grama.obj");
+    parser(chao_v, chao_t, chao_n, "vegetacao/grama.obj");
 
     glColor3f(0.13,0.55,0.13);
-    desenhaOBJ(out_vertices, out_texcoord, out_normals);
+    desenhaOBJ(chao_v, chao_t, chao_n);
 
     glPushMatrix();
         glColor3f(0,0,1);
-        glTranslated(1,800,1);
-        desenhaOBJ(out_vertices, out_texcoord, out_normals);
+        glTranslated(1,2000,1);
+        desenhaOBJ(chao_v, chao_t, chao_n);
     glPopMatrix();
+}
+
+void drawTree(){
+    parser(a1_v, a1_t, a1_n, ("vegetacao/arvore1.obj"));
+    parser(a2_v, a2_t, a2_n, ("vegetacao/arvore2.obj"));
+    parser(a3_v, a3_t, a3_n, ("vegetacao/arvore3.obj"));
+
+    glColor3f(1,1,1);
+    
+    glPushMatrix();
+        glTranslated(60,50,120);
+        desenhaOBJ(a1_v, a1_t, a1_n);
+    glPopMatrix();
+
+    glPushMatrix();
+        glTranslated(0,40,0);
+        desenhaOBJ(a2_v, a2_t, a2_n);
+    glPopMatrix();
+
+    glPushMatrix();
+        glTranslated(20,240,0);
+        desenhaOBJ(a3_v, a3_t, a3_n);
+    glPopMatrix();
+}
+
+void drawCastle(){
+    parser(c1_v, c1_t, c1_n, ("castelos/castle1.obj"));
+    //parser(c2_v, c2_t, c2_n, ("castelos/a.obj"));
+
+
+    glColor3f(1,0,0);
+    glPushMatrix();
+        glTranslated(4000,460,3000);
+        desenhaOBJ(c1_v, c1_t, c1_n);
+    glPopMatrix();
+
+    glPushMatrix();
+        glTranslated(3000,0,2000);
+        drawTree();
+    glPopMatrix();
+
+    glPushMatrix();
+        glTranslated(3000,240,3000);
+        desenhaOBJ(a3_v, a3_t, a3_n);
+        glTranslated(120,0,0);
+        desenhaOBJ(a3_v, a3_t, a3_n);
+    glPopMatrix();
+    //desenhaOBJ(c2_v, c2_t, c2_n);
+}
+
+void drawAnimals(){
+    parser(animal1_v, animal1_t, animal1_n, ("animais/charizard.obj"));
+    parser(animal2_v, animal2_t, animal2_n, ("animais/digimon.obj"));
+    parser(animal3_v, animal3_t, animal3_n, ("animais/umbreon.obj"));
+
+    glColor3f(1,1,0);
+    desenhaOBJ(animal1_v, animal1_t, animal1_n);
+    desenhaOBJ(animal2_v, animal2_t, animal2_n);
+    desenhaOBJ(animal3_v, animal3_t, animal3_n);
+}
+
+void drawM(){
+    parser(m1_v, m1_t, m1_n, ("montanha/montanha1.obj"));
+    parser(m2_v, m2_t, m2_n, ("montanha/montanha2.obj"));
+    
+    glColor3f(0,0,0);
+    glPushMatrix();
+    glTranslated(4500,600,-4200);
+    desenhaOBJ(m1_v, m1_t, m1_n);
+    glPopMatrix();
+
+    glPushMatrix();
+    glTranslated(4500,600,-2200);
+    desenhaOBJ(m1_v, m1_t, m1_n);
+    glPopMatrix();
+}
+
+void SpecialKeys(int key, int x, int y) {
+    if(key == GLUT_KEY_UP) {
+            velA ++;
+            std::cout << velA << "\n";
+        }
 }
 
 void init(void)
@@ -180,7 +311,7 @@ void init(void)
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
 
-    glFrustum(-40.0, 40.0, -40.0, 40.0, 30.0, 1200.0); //proje��o perspectiva
+    glFrustum(-40.0, 40.0, -40.0, 40.0, 30.0, 12000.0); //proje��o perspectiva
                                                       //proje��o perspectiva
 
     glMatrixMode(GL_MODELVIEW);
@@ -190,14 +321,18 @@ void display(void)
 {
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); //limpa a janela
     glLoadIdentity();
-    gluLookAt(300, 700, 300,       // define posicao do observador (x -> esquerda roda, y -> cima embaixo, z ->tras frente)
-              0.0, 0.0, 0.0, // ponto de interesse (foco)
+    gluLookAt(0, 300, 200 - velA,       // define posicao do observador (x -> esquerda roda, y -> cima embaixo, z ->tras frente)
+              0, 0, -velA, // ponto de interesse (foco)
               0.0, 1.0, 0.0);    // vetor de "view up"
 
 
     drawGramSky();
     drawAviao();
-    
+
+    drawCastle();
+    drawM();
+    glColor3f(0,1,0);
+    glTranslated(-5600,0,-3300);
 
     glBegin(GL_LINES);
         glColor3f(1,0,0); //coloquei isso daq so pra ficar mais legal de ver o desenho, se fosse tudo da mesma cor ia ficar ruim de visualizar
@@ -227,6 +362,8 @@ int main(int argc, char **argv)
     glutCreateWindow("Visualizacao 3D - Exemplo 1"); //cria a janela
     init();
     glutDisplayFunc(display); //determina fun��o callback de desenho
+    
+    glutSpecialFunc(SpecialKeys);
     glEnable(GL_DEPTH_TEST);  //habilita remo��o de superf�cies ocultas
     glEnable(GL_CULL_FACE);
     glCullFace(GL_BACK); // remove a parte de tras do cubo
